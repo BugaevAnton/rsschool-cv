10201c1ebb6128e8c028ed161edf058ae72adc59
BugaevAnton <buhayou.an@gmail.com>
2022-12-27 22:47:17 +0300

    corrected plants task

---
+++ plants/index.js
@@ -0,0 +1,7 @@
+const point1 = '1 10\n';
+const point2 = '2 20\n';
+const point3 = '3 48\n';
+const point4 = '4 12\n';
+const point5 = '5 20\n';
+
+console.log(point1, point2, point3, point4, point5);
