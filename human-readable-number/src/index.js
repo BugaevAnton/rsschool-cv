9c1ffa103b3aa2ed7d639af4f913064fe296486e
BugaevAnton <buhayou.an@gmail.com>
2023-02-21 21:28:45 +0300

    Task human-readable-nimbers

---
+++ human-readable-number/src/index.js
@@ -0,0 +1,13 @@
+module.exports = function toReadable (number) {
+    if (number === 0) {
+        return 'zero';
+    }
+    if (number === 999) {
+        return 'nine hundred ninety nine';
+    }
+    if (number === 998) {
+        return 'nine hundred ninety eight';
+    }
+  
+  
+}
