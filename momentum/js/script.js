24fafff49308501f6d82202f81ed616514f60aca
BugaevAnton <buhayou.an@gmail.com>
2023-02-22 20:34:10 +0300

    feat: add function change background

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,4 +1,4 @@
-//time start
+// time start
 const time = document.querySelector('.time')
 // console.log(currentTime);
 // console.log(date);
@@ -11,8 +11,8 @@ function showTime() {
     setTimeout(showTime, showDate, showGreeting, 1000);
 }
 showTime();
-//time end
-//date start
+// time end
+// date start
 const dateTime = document.querySelector('.date')
 const options = {month: 'long', weekday: 'long', day: 'numeric', timeZone: 'UTC'};
 
@@ -23,31 +23,34 @@ function showDate() {
     setTimeout(showDate, 1000);
 }
 showDate();
-//date end
-//greating start
-function showGreeting() {
+// date end
+
+// greating start
+
+function getTimeOfDay() {
     const date = new Date()
-    const greeting = document.querySelector('.greeting')
-    const hours = date.getHours()
-    
-        function getTimeOfDay() {
-            if(hours < 6){
-            return 'night'
-            } else if (hours < 12) { 
-                return 'morning'
-            } else if (hours < 18) {
-                return 'afternoon'
-            } else {
-                return 'evening'
-            }
+    const hours = date.getHours();
+    if(hours < 6){
+    return 'night'
+    } else if (hours < 12) { 
+        return 'morning'
+    } else if (hours < 18) {
+        return 'afternoon'
+    } else {
+        return 'evening'
     }
+};
+getTimeOfDay()
+
+function showGreeting() {
+    const greeting = document.querySelector('.greeting')
     const timeOfDay = getTimeOfDay();
     const greetingText =`Good ${timeOfDay}`;
     greeting.textContent = greetingText;
     setTimeout(showGreeting, 1000)
 };
 showGreeting()
-//greating end
+// greating end
 
 const userName = document.querySelector(".name");
 
@@ -66,19 +69,45 @@ function setLocalStorage() {
 //   setLocalStorage()
 //   getLocalStorage()
 
+// change background
 function getRandomNum() {
     return Math.floor(Math.random() * (21 - 1)) + 1;
 }
 
 function setBG() {
+    const img = new Image();
+    const body = document.querySelector('body');
     let timeOfDay = getTimeOfDay();
     let bgNum = String(getRandomNum()).padStart(2,'0');
-    const body = document.querySelector('body');
-    const img = new Image();
-    img.src = `https://github.com/BugaevAnton/stage1-tasks/tree/assets/images/${timeOfDay}/${bgNum}.jpg`;
+    img.src = `https://raw.githubusercontent.com/BugaevAnton/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
     img.onload = () => {
         body.style.backgroundImage = `url(${img.src})`
     };
 }
-
 setBG();
+
+const next = document.querySelector('.slide-next');
+const prev = document.querySelector('.slide-prev');
+
+
+next.addEventListener("click", getSlideNext)
+prev.addEventListener("click", getSlidePrev)
+
+let randomNum = getRandomNum;
+
+function getSlideNext() {
+    if(randomNum == 20) {
+        randomNum == 1;
+    } else {
+        randomNum += 1;
+    }
+    setBG()
+}
+function getSlidePrev() {
+    if(randomNum == 1) {
+        randomNum == 20
+    } else {
+        randomNum -= 1;
+    }
+    setBG()
+}
